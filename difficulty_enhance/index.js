8e7384e60504bf497b9e90e542f2d5ab6e46e785
WS-3917 <ws3917@outlook.com>
2024-06-09 01:35:34 +0800

    V23.7 - 繁体中文文本更新

--- difficulty_enhance/index.js
+++ difficulty_enhance/index.js
@@ -6,6 +6,6 @@ export default async function (mod, api) {
         SAVE,
         mechanics
     } = api;
-    mechanics.overrideHP = 1;
+    //mechanics.overrideHP = 1;
     //SAVE.data.n.hp = 1;
 }
